70bdadf513d0f51f1a5214ea88a8b121734a2e82
Harry Muriithi <hharryjjack@gmail.com>
2023-10-16 18:35:18 +0300

    Look my square, it’s perfect? No? Should I change something?

--- square.py
+++ square.py
@@ -1,24 +1,33 @@
 #!/usr/bin/python3
 
-class square():
-    
-    width = 0
-    height = 0
 
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+class square():
+    """
+    class representing a square
+
+    Args:
+        width(int) - the width of the square
+        height(int) - the height of the square
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        initializes width and height
+        """
+
+        self.width = width
+        self.height = height
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
     def PermiterOfMySquare(self):
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
